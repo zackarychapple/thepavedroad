81d706b691acf330e40b7ec434d36460e2b6603c
Omar Scott <omar@townsquaredigital.com>
2018-12-23 15:31:12 -0500

    handle mobile sizes for pill scroll

--- src/app/components/promo-content/promo-content.component.ts
+++ src/app/components/promo-content/promo-content.component.ts
@@ -26,13 +26,14 @@ export class PromoContentComponent {
       const elementPosition = element.nativeElement.getBoundingClientRect().top - halfHeightElem;
       // const startStep = window.innerHeight / 3.2;
       //const finishStep = window.innerHeight / 2.3;
-      const startStep = window.innerHeight / 3 + 200;
-      const finishStep = window.innerHeight / 3 - 200 ;
       // if (elementPosition > startStep  && elementPosition < finishStep) {
       //   this.renderer.addClass(element.nativeElement, 'active');
       // } else {
       //   this.renderer.removeClass(element.nativeElement, 'active');
       // }
+      const startStep = (window.innerWidth < 768 ) ? window.innerHeight / 3 + 100 : window.innerHeight / 3 + 200;
+      const finishStep = (window.innerWidth < 768 ) ? window.innerHeight / 3 - 100 : window.innerHeight / 3 - 200;
+
       if (elementPosition < startStep && elementPosition > finishStep) {
         this.renderer.addClass(element.nativeElement, 'active');
       } else {
